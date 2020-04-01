ffcad5c4d70cd8efc6b787d427592736bbb7394f
Oswaldo Neto <oswaldo@Oswaldos-MacBook-Pro.local>
2020-03-31 22:40:34 -0300

    Commit gateway

---
+++ service-discovery/src/main/java/digitalinnovation/one/experts/servicediscovery/ServiceDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package digitalinnovation.one.experts.servicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryApplication.class, args);
+	}
+
+}
